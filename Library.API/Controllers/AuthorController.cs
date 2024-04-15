50c4482226e7b19e778539a19c06f58ac71783a3
nelsyu <john211710@gmail.com>
2024-04-15 18:19:46 +0800

    4.3 創建Controller 新增Controllers

---
+++ Library.API/Controllers/AuthorController.cs
@@ -0,0 +1,17 @@
+﻿using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.API.Controllers
+{
+    [Route("api/authors")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        public AuthorController(IAuthorRepository authorRepository)
+        {
+            AuthorRepository = authorRepository;
+        }
+
+        public IAuthorRepository AuthorRepository { get; }
+    }
+}
