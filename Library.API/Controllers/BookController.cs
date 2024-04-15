d51bc22e1f7b72261bdae61a7204da2f31338c75
nelsyu <john211710@gmail.com>
2024-04-15 18:27:49 +0800

    4.4.3 獲取父/子形式的資源

---
+++ Library.API/Controllers/BookController.cs
@@ -0,0 +1,48 @@
+﻿using Library.API.Models;
+using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.API.Controllers
+{
+    [Route("api/authors/{authorId}/books")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        public IAuthorRepository AuthorRepository { get; }
+        public IBookRepository BookRepository { get; }
+
+        public BookController(IAuthorRepository authorRepository, IBookRepository bookRepository)
+        {
+            AuthorRepository = authorRepository;
+            BookRepository = bookRepository;
+        }
+
+        [HttpGet]
+        public ActionResult<List<BookDto>> GetBooks(Guid authorId)
+        {
+            if (!AuthorRepository.IsAuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            return BookRepository.GetBooksForAuthor(authorId).ToList();
+        }
+
+        [HttpGet("{bookId}")]
+        public ActionResult<BookDto> GetBook(Guid authorId, Guid bookId)
+        {
+            if (!AuthorRepository.IsAuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            var targetBook = BookRepository.GetBookForAuthor(authorId, bookId);
+            if (targetBook == null)
+            {
+                return NotFound();
+            }
+
+            return targetBook;
+        }
+    }
+}
