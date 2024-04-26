541ea12f4ac82a46d7cb0ddfe44c7f2c97fbcef4
nelsyu <john211710@gmail.com>
2024-04-26 23:58:48 +0800

    4.8.2 實現內容協商

--- Library.API/Program.cs
+++ Library.API/Program.cs
@@ -4,8 +4,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers()
-    .AddNewtonsoftJson();
+builder.Services.AddControllers(options =>
+{
+    options.ReturnHttpNotAcceptable = true;
+})
+    .AddNewtonsoftJson()
+    .AddXmlSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
