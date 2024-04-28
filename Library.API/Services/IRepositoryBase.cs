5e5c863eedfb4e47fc45080a56a43365c9e73d31
nelsyu <john211710@gmail.com>
2024-04-29 01:28:37 +0800

    5.3.1 創建通用倉儲接口

---
+++ Library.API/Services/IRepositoryBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+
+namespace Library.API.Services
+{
+    public interface IRepositoryBase<T>
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> GetByConditionAsync(Expression<Func<T, bool>> expression);
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        Task<bool> SaveAsync();
+    }
+
+    public interface IRepositoryBase2<T, TId>
+    {
+        Task<T?> GetByIdAsync(TId id);
+        Task<bool> IsExistAsync(TId id);
+    }
+}
